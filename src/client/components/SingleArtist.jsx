2ab6a5614f51c6a45c3f0cdbba3a1a7d58407c70
Garrett <garrett.curtis91@gmail.com>
2024-05-31 09:18:22 -0600

    Updated font and color scheme for all css

---
+++ src/client/components/SingleArtist.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom"
+
+const SingleArtist = () => {
+  return (
+    <>
+      <div>
+        <h3>Single Artist Page</h3>
+      </div>
+    </>
+  );
+}
+ 
+export default SingleArtist;

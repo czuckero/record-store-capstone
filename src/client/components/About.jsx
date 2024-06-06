c14cbf0618c0d72f2203959a7d22d38a2fe8e82a
Garrett <garrett.curtis91@gmail.com>
2024-06-06 08:51:49 -0600

    Updated color scheme and background image / added hover effects to Records

--- src/client/components/About.jsx
+++ src/client/components/About.jsx
@@ -7,13 +7,13 @@ const About = () => {
       <div className="about-container">
         <h1>About Us</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor vel velit a ullamcorper. Aenean venenatis risus in lectus convallis, nec auctor urna interdum. Duis id ex ac libero placerat gravida. 
+          Once upon a groovy time, in a land where bell-bottoms reigned supreme and disco balls glittered like tiny mirrored suns, three music-loving souls embarked on a far-out mission. Christian, the vinyl virtuoso with a hairdo that could rival any rock star's, teamed up with Christina, the disco queen whose dance moves were as smooth as her coding skills. Alongside them was Garrett, the tech wizard who could make computers sing and boogie. Together, they decided to spread the love of music beyond their funky basement hangout and into the wild, wild web by creating the ultimate online record store. They named it Groove Haven, a digital sanctuary where vinyl lovers could unite and find their next musical obsession.
         </p>
         <p>
-          Nulla facilisi. Phasellus convallis lacinia sapien, id vehicula metus varius in. Mauris accumsan tincidunt dui, ut feugiat arcu ultrices id. Morbi tristique lacinia massa, id sagittis velit vehicula ut. Integer vitae nisl vitae neque tempus malesuada. 
+          As the three amigos dug through crates of records, they reminisced about the good ol' days of mixtapes and Saturday night fever. Christina was in charge of curating the finest collection of albums, from classic rock anthems to the smoothest soul serenades. Garrett, with his retro laptop, brought the website to life with a design so slick, it could make even the stiffest square get down and boogie. Christian, always the man with a plan, ensured the site's backend was as solid as a Led Zeppelin riff, coding like a madman with fingers flying faster than a guitar solo. Their combined talents made Groove Haven the raddest online record store this side of the internet.
         </p>
         <p>
-          Sed vitae quam ut nisi sodales tincidunt. Cras at ligula metus. Fusce non lacinia elit, id ultrices orci. Praesent vel nulla ac sem suscipit eleifend. Curabitur a lacinia arcu. 
+          
         </p>
       </div>
     </>

558218fa468bcae9a9b60a42a659f888171bb830
Christian Zuckero <christianazuckero@gmail.com>
2024-06-11 11:32:42 -0400

    Worked on addToCart button

--- src/client/components/SingleRecord.jsx
+++ src/client/components/SingleRecord.jsx
@@ -7,6 +7,7 @@ const SingleRecord = ({ token }) => {
   const { recordId } = useParams();
   const [record, setRecord] = useState([]);
   const [message, setMessage] = useState("");
+  const [addedToCart, setAddedToCart] = useState(false)
 
   useEffect(() => {
     async function getSingleRecord() {
@@ -27,6 +28,7 @@ const SingleRecord = ({ token }) => {
         async function addItemToCart() {
           await addItemToUserCart(recordId, token, 1, record.price)
           console.log("added to cart");
+          setAddedToCart(true);
         }
         addItemToCart();
       } catch (error) {
@@ -55,7 +57,12 @@ const SingleRecord = ({ token }) => {
               Nulla facilisi. Phasellus convallis lacinia sapien, id vehicula metus varius in. Mauris accumsan tincidunt dui, ut feugiat arcu ultrices id. Morbi tristique lacinia massa, id sagittis velit vehicula ut. Integer vitae nisl vitae neque tempus malesuada.
             </p>
             <h3>{record.price}</h3>
-            <button onClick={() => handleAddToCart()} type="submit">Add to Cart</button>
+            {!addedToCart ? (
+              <button onClick={() => handleAddToCart()} type="submit">Add to Cart</button>
+            ) : (
+              <button disabled>Added to Cart!</button>
+            )
+            }
             {message && <h3>{message}</h3>}
           </div>
         </div>

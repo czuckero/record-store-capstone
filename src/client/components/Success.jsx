536568200a8f023d0f9e7e9c7416e0bc70a42321
Christian Zuckero <christianazuckero@gmail.com>
2024-06-13 12:05:38 -0400

    Simulated checkout and added checkout success page

--- src/client/components/Success.jsx
+++ src/client/components/Success.jsx
@@ -1,6 +1,10 @@
 const Success = () => {
   return (
-    <h1>You have successfully checked out!</h1>
+    <>
+      <h1>You have successfully checked out!</h1>
+      <h2>You're order total is</h2>    
+    </>
+
   );
 }
  
